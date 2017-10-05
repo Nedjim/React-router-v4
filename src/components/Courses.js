e4b91682318b30db302a26aaa55e90217a6cdc4b
Nedjim <nedjim.danimon@gmail.com>
2017-10-05 11:10:35 +0200

    Add match props in submenu

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -4,19 +4,19 @@ import Course1 from './Courses/Course1';
 import Course2 from './Courses/Course2';
 import Course3 from './Courses/Course3';
 
-const Courses = () => (
+const Courses = ({match}) => (
     <div>
         <h1>Courses</h1>
         <nav>
-            <NavLink to='/courses/course1'>Course 1</NavLink>
-            <NavLink to='/courses/course2'>Course 2</NavLink>
-            <NavLink to='/courses/course3'>Course 3</NavLink>
+            <NavLink to={`${match.url}/course1`}>Course 1</NavLink>
+            <NavLink to={`${match.url}/course2`}>Course 2</NavLink>
+            <NavLink to={`${match.url}/course3`}>Course 3</NavLink>
         </nav>
         <Switch>
-            <Route exact path='/courses' render={ () => <Redirect to="/courses/course1"/> }/>
-            <Route path='/courses/course1' component={Course1}/>
-            <Route path='/courses/course2' component={Course2}/>
-            <Route path='/courses/course3' component={Course3}/>
+            <Route exact path={match.path} render={ () => <Redirect to={`${match.path}/course1`}/> }/>
+            <Route path={`${match.path}/course1`} component={Course1}/>
+            <Route path={`${match.path}/course2`} component={Course2}/>
+            <Route path={`${match.path}/course3`} component={Course3}/>
         </Switch>
     </div>
 );
